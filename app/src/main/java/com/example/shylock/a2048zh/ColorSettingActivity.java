e7d9d73433d06e86d97e91e7d92f14f10731cdc6
Shylock <123>
2018-05-17 09:17:12 +0800

    5-17-1 setting1

---
+++ app/src/main/java/com/example/shylock/a2048zh/ColorSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.shylock.a2048zh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_setting);
+    }
+}
--- app/src/main/java/com/example/shylock/a2048zh/MainActivity.java
+++ app/src/main/java/com/example/shylock/a2048zh/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.shylock.a2048zh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button setting_btn;
     public MainActivity(){
         mainActivity = this;
     }
@@ -15,18 +23,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setting_btn = findViewById(R.id.setting_btn);
         tvScore = (TextView) findViewById(R.id.tvScore);
+        popliset();
     }
 
     public void clearScore(){
         score = 0;
         showScore();
     }
-
     public void showScore(){
         tvScore.setText(score + "");
     }
-
     public void addScore(int s){
         score += s;
         showScore();
@@ -40,4 +48,34 @@ public class MainActivity extends AppCompatActivity {
     public static MainActivity getMainActivity(){
         return mainActivity;
     }
+
+    public void popliset(){
+        setting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,view);
+                popupMenu.inflate(R.menu.main_menu);
+                popupMenu.show();
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.music:
+                                Toast.makeText(MainActivity.this,"pop "+getResources().getString(R.string.music_seting),Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.color:
+                                Toast.makeText(MainActivity.this,"pop "+getResources().getString(R.string.color_setting),Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.clear:
+                                Toast.makeText(MainActivity.this,"pop "+getResources().getString(R.string.clear_note),Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        return true;
+                    }
+                });
+            }
+        });
+    }
+
 }
