6aded501f5a90d30ecd1625a9698ca920c8097a4
Shylock <123>
2018-05-17 18:55:19 +0800

    5-17-3 setting 2 color-ok

--- app/src/main/java/com/example/shylock/a2048zh/ColorSettingActivity.java
+++ app/src/main/java/com/example/shylock/a2048zh/ColorSettingActivity.java
@@ -8,21 +8,59 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ColorSettingActivity extends AppCompatActivity {
 
     int[] colors = {Color.rgb(187,173,160),Color.rgb(255,106,106),Color.rgb(144,238,144),Color.rgb(30,144,255),Color.rgb(238,221,130),Color.rgb(139,137,137)};
-    String[] string_color = {"默认","粉色","绿色","蓝色","黄色","灰色"};
+    String[] string_color = {"默认","红色","绿色","蓝色","黄色","灰色"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_setting);
+
         ListView listView = findViewById(R.id.listView);
 
+        ArrayList<HashMap<String,Object>> arrayList = new ArrayList<>();
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("color","默认");
+        hashMap.put("pic",R.drawable.defaultcol);
+        arrayList.add(hashMap);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,string_color);
+        hashMap = new HashMap<>();
+        hashMap.put("color","红色");
+        hashMap.put("pic",R.drawable.red);
+        arrayList.add(hashMap);
 
-        listView.setAdapter(arrayAdapter);
+        hashMap = new HashMap<>();
+        hashMap.put("color","绿色");
+        hashMap.put("pic",R.drawable.green);
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("color","蓝色");
+        hashMap.put("pic",R.drawable.blue);
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("color","黄色");
+        hashMap.put("pic",R.drawable.yellow);
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("color","灰色");
+        hashMap.put("pic",R.drawable.grey);
+        arrayList.add(hashMap);
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,arrayList,R.layout.colorsettinglayout,new String[]{"color","pic"},new int[]{R.id.textView_color,R.id.imageView});
+        listView.setAdapter(simpleAdapter);
+
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,string_color);
+//
+//        listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
